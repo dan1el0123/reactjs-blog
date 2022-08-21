22a088732061a218434a419241c10ab951d31f59
Daniel <conevskidaniel1999@gmail.com>
2022-08-21 22:50:22 +0200

    add about and missing page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,12 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This Blog app is a project using Context API, React Router and hooks.
+      </p>
+    </main>
+  );
 };
 
 export default About;
