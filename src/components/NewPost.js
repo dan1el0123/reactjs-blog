0afa37299d2573d640b42e8caaaa79cb58d44f76
Daniel <conevskidaniel1999@gmail.com>
2022-08-22 11:16:16 +0200

    add and style NewPost component

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,5 +1,62 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+import api from "../api/posts";
+import DataContext from "../context/DataContext";
+
 const NewPost = () => {
-  return <div>NewPost</div>;
+  const { posts, setPosts } = useContext(DataContext);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id,
+      title: postTitle,
+      datetime,
+      body: postBody,
+    };
+    try {
+      const response = await api.post("/posts", newPost);
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostBody("");
+      setPostTitle("");
+      navigate(`/post/${id}`);
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  return (
+    <main className="NewPost">
+      <h2>New Post</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          placeholder="Enter post title"
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+          required
+        />
+        <label htmlFor="postBody">Body:</label>
+        <textarea
+          id="postBody"
+          placeholder="Enter post body"
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 };
 
 export default NewPost;
