c01f509ad7d8d00398c8cb40cd6919277f13f901
Daniel <conevskidaniel1999@gmail.com>
2022-08-22 12:03:56 +0200

    add edit post functionality

--- src/components/EditPost.js
+++ src/components/EditPost.js
@@ -1,5 +1,84 @@
+import { useContext, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import api from "../api/posts";
+import { format } from "date-fns";
+import DataContext from "../context/DataContext";
+
 const EditPost = () => {
-  return <div>EditPost</div>;
+  const { posts, setPosts } = useContext(DataContext);
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (post) {
+      setEditBody(post.body);
+      setEditTitle(post.title);
+    }
+  }, [post]);
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const updatedPost = {
+      id,
+      title: editTitle,
+      datetime,
+      body: editBody,
+    };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      const postsList = posts.map((post) =>
+        post.id === id ? { ...response.data } : post
+      );
+      setPosts(postsList);
+      setEditBody("");
+      setEditTitle("");
+      navigate(`/post/${id}`);
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  return (
+    <main className="NewPost">
+      {editTitle && (
+        <>
+          <h2>Edit Post</h2>
+          <form className="newPostForm" onSubmit={(e) => e.preventDefault()}>
+            <label htmlFor="postTitle">Title:</label>
+            <input
+              type="text"
+              id="postTitle"
+              required
+              value={editTitle}
+              onChange={(e) => setEditTitle(e.target.value)}
+            />
+            <label htmlFor="postBody">Body:</label>
+            <textarea
+              id="postBody"
+              required
+              value={editBody}
+              onChange={(e) => setEditBody(e.target.value)}
+            />
+            <button type="submit" onClick={() => handleEdit(post.id)}>
+              Submit
+            </button>
+          </form>
+        </>
+      )}
+      {!editTitle && (
+        <>
+          <h2>Post Not Found</h2>
+          <p>Well, that's dissapointing</p>
+          <p>
+            <Link to="/">Visit our HomePage</Link>
+          </p>
+        </>
+      )}
+    </main>
+  );
 };
 
 export default EditPost;
