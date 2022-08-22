2fd09ed003a8162e67046a8965862d50a4143a2f
Daniel <conevskidaniel1999@gmail.com>
2022-08-22 10:35:18 +0200

    fix data fetch running twice in development

--- src/hooks/useAxiosFetch.js
+++ src/hooks/useAxiosFetch.js
@@ -1,32 +1,38 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const useAxiosFetch = (dataUrl) => {
   const [data, setData] = useState([]);
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const fetchRef = useRef(false);
 
   useEffect(() => {
     const controller = new AbortController();
-    const fetchData = async (url) => {
-      setIsLoading(true);
-      try {
-        const response = await axios.get(url, {
-          signal: controller.signal,
-        });
-        setData(response.data);
-        setFetchError(null);
-      } catch (err) {
-        setFetchError(err.message);
-        setData([]);
-      } finally {
-        setIsLoading(false);
-      }
+    if (fetchRef.current === true || process.env.NODE_ENV !== "development") {
+      const fetchData = async (url) => {
+        setIsLoading(true);
+        try {
+          const response = await axios.get(url, {
+            signal: controller.signal,
+          });
+          setData(response.data);
+          setFetchError(null);
+        } catch (err) {
+          setFetchError(err.message);
+          setData([]);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+
+      fetchData(dataUrl);
+    }
+
+    return () => {
+      fetchRef.current = true;
+      controller.abort();
     };
-
-    fetchData(dataUrl);
-
-    return () => controller.abort();
   }, [dataUrl]);
 
   return { data, fetchError, isLoading };
